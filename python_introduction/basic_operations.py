b480af9701c5498b615b23b52ce587a92db98e8e
Oluwaseyi <oluwaseyipd@gmail.com>
2025-05-19 11:15:57 +0100

    0. Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+#variables
+number1 = 24
+number2 = 4
+
+#addition
+print(f"Addition of {number1} and {number2} is {number1 + number2}.")
+#subtraction
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}.")
+#multiplication
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}.")
