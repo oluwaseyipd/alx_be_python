6908f0d1a3c02f7d23ae691c7d725113102d808c
Oluwaseyi <oluwaseyipd@gmail.com>
2025-05-19 11:39:09 +0100

    All mandatory tasks.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,10 @@
 # Simple Interest Calculator
-# Formular for Simple Interest: SI = (P * R * T) / 100
+# Formular for Interest: SI = (P * R * T)
 
 principal = 1000
 rate = 0.05
 time = 3
 
-interest = (principal * rate * time) / 100
+interest = (principal * rate * time)
+
 print(f"The Simple Interest is: {interest}")
